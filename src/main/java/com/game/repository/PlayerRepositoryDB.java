a3c3a2bc7bec1d1699a71680875efcfeaa7026a1
Radomir Gross <grossbrn1990@gmail.com>
2024-03-31 13:53:15 +0700

    second commit

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,22 +1,53 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.NativeQuery;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PreDestroy;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
+private final SessionFactory sessionFactory;
     public PlayerRepositoryDB() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "org.postgresql.Driver");
+        properties.put(Environment.URL, "jdbc:postgresql://localhost:5433/rpg");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put(Environment.SHOW_SQL, "true");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
 
+        sessionFactory=new Configuration()
+                .addAnnotatedClass(Player.class)
+                .setProperties(properties)
+                .buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        try(Session session= sessionFactory.openSession())
+        {
+            NativeQuery query=session.createNativeQuery("select * from rpg.player", Player.class);
+            query.setFirstResult(pageNumber*pageSize);
+            query.setMaxResults(pageSize);
+            return query.getResultList();
+
+
+
+        }
+
+
+
     }
 
     @Override
@@ -44,8 +75,8 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     }
 
-    @PreDestroy
-    public void beforeStop() {
+ //   @PreDestroy
+   // public void beforeStop() {
 
-    }
+  //  }
 }
